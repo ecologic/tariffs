2d2b770e2d24e82fd7be994cc096cd49a3318ab3
John McKibbin <john.mckibbin@gmail.com>
2018-07-26 15:50:56 +1000

    Change test tariffs from dicts to json fixtures
    This is a more typical usage of the library so more helpful for example use cases.

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,6 +1,6 @@
 from tariffs.tariff import Tariff
 import pytest
-from odin.codecs import dict_codec
+from odin.codecs import json_codec
 import pandas
 import datetime
 
@@ -18,181 +18,46 @@ class TestTariff(object):
         return meter_data
 
     @pytest.fixture
+    def tariff_file(self):
+        with open('./fixtures/tariff_multi_BTOWN.json') as f:
+            tariff = json_codec.load(f, Tariff)
+        return tariff
+
+    @pytest.fixture
     def block_tariff(self):
-        block_tariff = dict_codec.load(
-            {
-                "charges": [
-                    {
-                        "rate_bands": [
-                            {
-                                "limit": 10,
-                                "rate": 1.0
-                            },
-                            {
-                                "rate": 1.0
-                            }
-                        ]
-                    }
-                ]
-            }, Tariff
-        )
-        return block_tariff
+        with open('./fixtures/block_tariff.json') as f:
+            tariff = json_codec.load(f, Tariff)
+        return tariff
 
     @pytest.fixture
     def seasonal_tariff(self):
-        seasonal_tariff = dict_codec.load(
-            {
-                "charges": [
-                    {
-                        "rate": 1.0,
-                        "season": {
-                            "name": "summer",
-                            "from_month": 1,
-                            "from_day": 1,
-                            "to_month": 3,
-                            "to_day": 31
-                        }
-                    },
-                    {
-                        "rate": 1.0,
-                        "season": {
-                            "name": "winter",
-                            "from_month": 4,
-                            "from_day": 1,
-                            "to_month": 12,
-                            "to_day": 31
-                        }
-                    }
-                ]
-            }, Tariff
-        )
-        return seasonal_tariff
+        with open('./fixtures/seasonal_tariff.json') as f:
+            tariff = json_codec.load(f, Tariff)
+        return tariff
 
     @pytest.fixture
     def tou_tariff(self):
-        tou_tariff = dict_codec.load(
-            {
-                "charges": [
-                    {
-                        "rate": 1.0,
-                        "time": {
-                            "name": "peak",
-                            "periods": [
-                                {
-                                    "from_weekday": 0,
-                                    "to_weekday": 4,
-                                    "from_hour": 14,
-                                    "to_hour": 19,
-                                }
-                            ]
-                        }
-                    },
-                    {
-                        "rate": 1.0,
-                        "time": {
-                            "name": "shoulder",
-                            "periods": [
-                                {
-                                    "from_weekday": 0,
-                                    "to_weekday": 4,
-                                    "from_hour": 10,
-                                    "to_hour": 13,
-                                },
-                                {
-                                    "from_weekday": 0,
-                                    "to_weekday": 4,
-                                    "from_hour": 20,
-                                    "to_hour": 21,
-                                }
-                            ]
-                        }
-                    },
-                    {
-                        "rate": 1.0,
-                        "time": {
-                            "name": "off-peak",
-                            "periods": [
-                                {
-                                    "from_weekday": 0,
-                                    "to_weekday": 4,
-                                    "from_hour": 0,
-                                    "from_minute": 0,
-                                    "to_hour": 9,
-                                    "to_minute": 59
-                                },
-                                {
-                                    "from_weekday": 0,
-                                    "to_weekday": 4,
-                                    "from_hour": 22,
-                                    "from_minute": 0,
-                                    "to_hour": 23,
-                                    "to_minute": 59
-                                },
-                                {
-                                    "from_weekday": 5,
-                                    "to_weekday": 6
-                                }
-                            ]
-                        }
-                    }
-                ]
-            }, Tariff
-        )
-        return tou_tariff
+        with open('./fixtures/tou_tariff.json') as f:
+            tariff = json_codec.load(f, Tariff)
+        return tariff
 
     @pytest.fixture
     def scheduled_tariff(self):
-        scheduled_tariff = dict_codec.load(
-            {
-                "charges": [
-                    {
-                        "rate_schedule": [
-                            {
-                                "datetime":"2018-01-01T00:00:00Z",
-                                "rate": 1.0
-                            },
-                            {
-                                "datetime":"2018-06-01T00:30:00Z",
-                                "rate": 1.0
-                            },
-                            {
-                                "datetime": "2018-12-31T01:00:00Z",
-                                "rate": 1.0
-                            }
-                        ]
-                    }
-                ]
-            }, Tariff
-        )
-        return scheduled_tariff
+        with open('./fixtures/scheduled_tariff.json') as f:
+            tariff = json_codec.load(f, Tariff)
+        return tariff
 
     @pytest.fixture
     def supply_payment_tariff(self):
-        supply_payment_tariff = dict_codec.load(
-            {
-                "charges": [
-                    {
-                        "rate": -1.0,
-                        "type": "generation"
-                    }
-                ]
-            }, Tariff
-        )
-        return supply_payment_tariff
+        with open('./fixtures/supply_payment.json') as f:
+            tariff = json_codec.load(f, Tariff)
+        return tariff
 
     @pytest.fixture
     def demand_tariff(self):
-        demand_tariff = dict_codec.load(
-            {
-                "charges": [
-                    {
-                        "rate": 1.0,
-                        "type": "demand"
-                    }
-                ]
-            }, Tariff
-        )
-        return demand_tariff
+        with open('./fixtures/demand_tariff.json') as f:
+            tariff = json_codec.load(f, Tariff)
+        return tariff
 
     def test_block_tariff(self, block_tariff, meter_data):
         expected_bill = 35040.0
